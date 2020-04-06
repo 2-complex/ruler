663b39442bf6d312fd109c74c911b1f0c5077cf5
Peterson Trethewey <peterson.trethewey@gmail.com>
2020-04-05 22:41:12 -0700

    Commented ticket.rs

--- src/ticket.rs
+++ src/ticket.rs
@@ -10,6 +10,9 @@ use filesystem::FileSystem;
 use std::fmt;
 use std::io::Read;
 
+/*  Ticket is a struct representing a hash of a file or a rule.  To construct a ticket,
+    you first make a TiketFactory, and you can feed the factory data bit by bit for it to
+    hash, using functions that start with "input_" then get the ticket using result(). */
 pub struct TicketFactory
 {
     dig : Sha512
@@ -17,11 +20,14 @@ pub struct TicketFactory
 
 impl TicketFactory
 {
+    /*  Create an empty TicketFactory initialized with no bytes. */
     pub fn new() -> TicketFactory
     {
         TicketFactory{ dig : Sha512::new() }
     }
 
+    /*  Construct a TicketFactory immediately reading in
+        the bytes of the given string as input. */
     #[cfg(test)]
     pub fn from_str(first_input: &str) -> TicketFactory
     {
@@ -30,16 +36,21 @@ impl TicketFactory
         TicketFactory{ dig : d }
     }
 
+    /*  Read in a Ticket, convert (the hash therein) to bytes,
+        and incorporate those bytes into the currently building ticket. */
     pub fn input_ticket(&mut self, input: Ticket)
     {
         self.dig.input(&input.sha);
     }
 
+    /*  Read in a str, convert to bytes, and incorporate those bytes
+        into the currently building ticket. */
     pub fn input_str(&mut self, input: &str)
     {
         self.dig.input(input.as_bytes());
     }
 
+    /*  Create a ticket from the bytes incorporated so far. */
     pub fn result(&mut self) -> Ticket
     {
         let mut out_sha = vec![0u8; 64];
@@ -50,12 +61,13 @@ impl TicketFactory
         }
     }
 
-    pub fn from_file<FSType: FileSystem>(
+    /*  Construct a TicketFactory, initialized with the contents of a file
+        from a FileSystem */
+    pub fn from_file<FSType: FileSystemß>(
         file_system: &FSType,
         path : &str)
         -> Result<TicketFactory, std::io::Error>
     {
-
         match file_system.open(path)
         {
             Ok(mut reader) =>
@@ -83,6 +95,7 @@ impl TicketFactory
     }
 }
 
+/*  Ticket represents a hash of a file or a rule */
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub struct Ticket
 {
@@ -91,11 +104,14 @@ pub struct Ticket
 
 impl Ticket
 {
+    /*  Returns a string URL-safe base64-encoded hash */
     pub fn base64(&self) -> String
     {
         format!("{}", encode_config(&self.sha, base64::URL_SAFE))
     }
 
+    /*  Use this function to create a ticket based on the targets, sources and command
+        of a rule. */
     pub fn from_strings(
         targets: &Vec<String>,
         sources: &Vec<String>,
@@ -156,6 +172,8 @@ mod test
     use filesystem::{FileSystem, FakeFileSystem};
     use lipsum::{LOREM_IPSUM};
 
+    /*  Uses a TicketFactory to construct a Ticket based on a single string with one character,
+        compares with exemplar. */
     #[test]
     fn ticket_factory_string()
     {
@@ -165,14 +183,19 @@ mod test
             "Umd2iCLuYk1I_OFexcp5y9YCy39MIVelFlVpkfIu-Me173sY0f9BxZNw77CFhlHUSpNsEbexRMSP4E3zxqPo2g==");
     }
 
+    /*  Uses a TicketFactory to construct a Ticket based on a single string with more than one character,
+        compares with exemplar. */
     #[test]
-    fn ticket_from_string2()
+    fn ticket_from_string_more()
     {
         let ticket = TicketFactory::from_str("Time wounds all heels.\n").result();
         assert_eq!(ticket.base64(),
             "PRemaMHXvOuGAx87EOGZY1_cGUv4udBiqVmgP8nwVX93njjGOdE41zf4rV9PAbiJp_i6ucukKrvFp3zldP42wA==");
     }
 
+    /*  Constructs two tickets for the same string, A: by calling input_str with pieces of the string,
+        and B: by taking the whole string at once in from_str().  Checks that the tickets are the same,
+        and equal to an exemplar. */
     #[test]
     fn ticket_from_string_from_new()
     {
@@ -181,11 +204,17 @@ mod test
         factory.input_str("wounds ");
         factory.input_str("all ");
         factory.input_str("heels.\n");
-        let ticket = factory.result();
-        assert_eq!(ticket.base64(),
+        let ticketA = factory.result();
+        let ticketB = TicketFactory::from_str("Time wounds all heels.\n").result();
+
+        assert_eq!(ticketA.base64(),
             "PRemaMHXvOuGAx87EOGZY1_cGUv4udBiqVmgP8nwVX93njjGOdE41zf4rV9PAbiJp_i6ucukKrvFp3zldP42wA==");
+
+        assert_eq!(ticketA.base64(), ticketB.base64());
     }
 
+    /*  Using a fake file system, create a file, populate with some known text, use TicketFactory::from_file
+        to get a hash and compare with an exemplar.  */
     #[test]
     fn ticket_factory_file()
     {
@@ -207,6 +236,8 @@ mod test
         }
     }
 
+    /*  Using a fake filesystem, create a file, populate it with with known text, then use TicketFactory::from_str
+        and input_ticket to simulate making a ticket with that file as a target.  Compare the hash with an exemplar.*/
     #[test]
     fn ticket_factory_hashes()
     {
@@ -230,6 +261,8 @@ mod test
         }
     }
 
+    /*  Obtain lorem ipsum, and write it to a file in a fake filesystem.  Then use TicketFactory::from_file
+        to generate a ticket.  Compare hash with an exemplar. */
     #[test]
     fn ticket_factory_hashes_bigger_file()
     {
@@ -254,6 +287,8 @@ mod test
         }
     }
 
+    /*  Make a ticket, serialize to a vector of bytes, then deserialize, and check that
+        the deserialized ticket matches the original. */
     #[test]
     fn ticket_serialize_round_trip()
     {
@@ -261,5 +296,6 @@ mod test
         let encoded: Vec<u8> = bincode::serialize(&ticket).unwrap();
         let decoded: Ticket = bincode::deserialize(&encoded[..]).unwrap();
         assert_eq!(ticket, decoded);
+        assert_eq!(ticket.base64(), decoded.base64());
     }
 }
