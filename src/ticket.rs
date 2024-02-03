791bdc07b9e99bd3f6a56eba6da33e27b438adc5
Peterson Trethewey <peterson.trethewey@gmail.com>
2024-02-03 15:47:50 -0800

    Progress

--- src/ticket.rs
+++ src/ticket.rs
@@ -20,6 +20,65 @@ use crate::system::
 use std::fmt;
 use std::io::Read;
 
+use num_bigint::
+{
+    BigUint
+};
+
+use num_traits::identities::{Zero, One};
+
+enum Decode62Error
+{
+    InvalidCharacter
+}
+
+fn decode62(tag : &str) -> Result<BigUint, Decode62Error>
+{
+    let mut result = BigUint::zero();
+    let mut d = BigUint::one();
+    for c in tag.chars()
+    {
+        result += &d *
+        match c
+        {
+            '0' => 0u32, '1' => 1, '2' => 2, '3' => 3, '4' => 4, '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9, 'a' => 10,
+            'b' => 11, 'c' => 12, 'd' => 13, 'e' => 14, 'f' => 15, 'g' => 16, 'h' => 17, 'i' => 18, 'j' => 19, 'k' => 20, 'l' => 21, 'm' => 22, 'n' => 23, 'o' => 24, 'p' => 25, 'q' => 26, 'r' => 27, 's' => 28, 't' => 29, 'u' => 30, 'v' => 31, 'w' => 32, 'x' => 33, 'y' => 34, 'z' => 35,
+            'A' => 36, 'B' => 37, 'C' => 38, 'D' => 39, 'E' => 40, 'F' => 41, 'G' => 42, 'H' => 43, 'I' => 44, 'J' => 45, 'K' => 46, 'L' => 47, 'M' => 48, 'N' => 49, 'O' => 50, 'P' => 51, 'Q' => 52, 'R' => 53, 'S' => 54, 'T' => 55, 'U' => 56, 'V' => 57, 'W' => 58, 'X' => 59, 'Y' => 60, 'Z' => 61,
+            _ =>
+            {
+                return Err(Decode62Error::InvalidCharacter);
+            },
+        };
+        d *= 62u32;
+    }
+
+    Ok(result)
+}
+
+fn encode62(mut n: BigUint) -> String
+{
+    // 0-9, a-z, A-Z
+    let alphabet : [u8; 62] = [
+        48, 49, 50, 51, 52, 53, 54, 55, 56, 57,
+        97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122,
+        65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90
+    ];
+
+    let m = alphabet.len();
+
+    let mut buffer = [48u8; 43];
+    let mut i = 0;
+    while n > BigUint::zero()
+    {
+        buffer[i] = alphabet[
+            (&n % m).iter_u32_digits().next().unwrap() as usize];
+        i+=1;
+        n /= m;
+    }
+
+    std::str::from_utf8(&buffer).unwrap().to_string()
+}
+
 /*  Ticket is a struct representing a hash of a file or a rule.  To construct a ticket,
     you first make a TiketFactory, and you can feed the factory data bit by bit for it to
     hash, using functions that start with "input_" then get the ticket using result(). */
@@ -114,7 +173,6 @@ impl TicketFactory
     ->
     Result<TicketFactory, ReadWriteError>
     {
-        let mut factory = TicketFactory::new();
 
         let path_list =
         match system.list_dir(path)
@@ -123,6 +181,7 @@ impl TicketFactory
             Err(_error) => return Err(ReadWriteError::SystemError(SystemError::NotFound)),
         };
 
+        let mut factory = TicketFactory::from_str(&path_list.join("\n"));
         for path in path_list
         {
             if system.is_dir(&path)
@@ -485,6 +544,27 @@ mod test
         system.create_dir("time-files-1").unwrap();
         write_str_to_file(&mut system, "time-files-0/time0.txt", "Time wounds all heels.\n").unwrap();
         write_str_to_file(&mut system, "time-files-1/time1.txt", "Time: March is on.\n").unwrap();
+
+        let ticket0 = TicketFactory::from_directory(&system, "time-files-0").unwrap().result();
+        let ticket1 = TicketFactory::from_directory(&system, "time-files-1").unwrap().result();
+
+        assert!(hash_heuristic(&ticket0.human_readable()));
+        assert!(hash_heuristic(&ticket1.human_readable()));
+
+        assert_ne!(ticket0, ticket1)
+    }
+
+    /*  Using a fake file-system, create two directories, populate with some known text, use TicketFactory::from_file
+        to get a hash and compare with an exemplar.  */
+    #[test]
+    fn ticket_factory_two_directories_different_names()
+    {
+        let mut system = FakeSystem::new(10);
+        system.create_dir("time-files-0").unwrap();
+        system.create_dir("time-files-1").unwrap();
+        let content = "Time wounds all heels.\n";
+        write_str_to_file(&mut system, "time-files-0/time0.txt", content).unwrap();
+        write_str_to_file(&mut system, "time-files-1/time1.txt", content).unwrap();
 
         let ticket0 = TicketFactory::from_directory(&system, "time-files-0").unwrap().result();
         let ticket1 = TicketFactory::from_directory(&system, "time-files-1").unwrap().result();
