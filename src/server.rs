46c69bc9040ce9643a1d3f57c806e1513c02c4f4
Peterson Trethewey <peterson.trethewey@gmail.com>
2024-09-30 20:30:11 -0700

    upload endpoint works, but doesn't write to the right place yet

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use std::fs;
 use std::io::Read;
 use std::net::
 {
@@ -7,37 +8,33 @@ use std::net::
     Ipv4Addr
 };
 
-use warp::http::
-{
-    Response,
-    StatusCode,
-};
-
-use warp::multipart::form;
-
+use warp::http::Response;
+use warp::http::StatusCode;
+use warp::multipart::FormData;
+use warp::Reply;
+use warp::Rejection;
+use warp::Filter;
 use crate::directory;
 use crate::ticket::Ticket;
-use warp::Filter;
-use warp::multipart::FormData;
 use futures::TryStreamExt;
 use futures::StreamExt;
-use futures::executor::block_on;
-use crate::cache::
-{
-    SysCache,
-};
+use crate::cache::SysCache;
+use crate::history::History;
+use crate::system::System;
+use std::path::PathBuf;
+use std::path::Path;
+use bytes::buf::Buf;
 
-use crate::system::
-{
-    System
-};
 
+fn to_path_buf(path: &str) -> PathBuf
+{
+    Path::new(".").join(path.split("/").map(|s|{s.to_string()}).collect::<PathBuf>())
+}
 
 pub enum ServerError
 {
     Weird,
 }
-
 
 impl fmt::Display for ServerError
 {
@@ -51,60 +48,90 @@ impl fmt::Display for ServerError
     }
 }
 
-async fn process_upload<SystemType :  System>(cache: SysCache<SystemType>, form: FormData)
+fn get_history_endpoint<SystemType : System + Clone + Send + 'static>
+(history : History<SystemType>) -> warp::filters::BoxedFilter<(impl warp::Reply,)>
 {
-    let mut parts = form.into_stream();
-    loop
-    {
-        match parts.next().await
-        {
-            Some(part_result) =>
+    warp::get()
+        .and(warp::path!("history"))
+        .map(
+            move||
             {
-                match part_result
+                let message =
+                match history.list()
                 {
-                    Ok(mut p) =>
+                    Ok(rule_hashes) =>
                     {
-                        println!("------Part Received-------");
-                        println!("name: {:?}", p.name());
-                        println!("filename: {:?}", p.filename());
-                        println!("content-type: {:?}", p.content_type());
+                        let mut message_vec = vec!["<html><head></head><body style = \"font-family: monospace;\">".to_string()];
+                        for hash in rule_hashes
+                        {
+                            message_vec.push(format!("<a href= \"rule_history/{}\">{}</a>", hash, hash));
+                        }
+                        message_vec.push("</body></html>".to_string());
+                        message_vec.join("\n")
                     },
-
-                    Err(err) =>
+                    Err(error) =>
                     {
-                        println!("{:?}", err);
-                    },
-                }
-            },
+                        format!("{}", error)
+                    }
+                };
 
-            None => break,
-        }
-    }
+                Response::builder()
+                    .status(StatusCode::OK)
+                    .body(message)
+            }
+        ).boxed()
 }
 
-#[tokio::main]
-pub async fn serve
-<
-    SystemType : System + Clone + Send + 'static,
->
-(
-    mut system : SystemType,
-    directory_path : &str,
-    address : Ipv4Addr,
-    port : u16
-)
--> Result<(), ServerError>
+fn get_rule_history_endpoint<SystemType : System + Clone + Send + 'static>
+(history : History<SystemType>) -> warp::filters::BoxedFilter<(impl warp::Reply,)>
 {
-    let elements =
-    match directory::init(&mut system, directory_path)
-    {
-        Ok(elements) => elements,
-        Err(error) => panic!("Failed to init directory error: {}", error)
-    };
+    warp::get()
+        .and(warp::path!("rule_history" / String))
+        .map(
+            move|rule_hash_str : String|
+            {
+                let rule_ticket =
+                match Ticket::from_human_readable(&rule_hash_str)
+                {
+                    Ok(ticket) => ticket,
+                    Err(error) =>
+                    {
+                        return Response::builder()
+                            .status(StatusCode::NOT_FOUND)
+                            .body(format!("Error: {}", error).into_bytes())
+                    }
+                };
 
-    let cache = elements.cache;
+                let rule_history =
+                match history.read_rule_history(&rule_ticket)
+                {
+                    Ok(rule_history) => rule_history,
+                    Err(error) => return
+                        Response::builder()
+                            .status(StatusCode::NOT_FOUND)
+                            .body(format!("Error: {}", error).into_bytes()),
+                };
 
-    let files_endpoint = warp::get()
+                let mut message_vec = vec!["<html><head></head><body style = \"font-family: monospace;\">".to_string()];
+                let source_to_targets = &rule_history.get_source_to_targets();
+
+                for (source_ticket, file_state_vec) in source_to_targets.iter()
+                {
+                    let file_state_vec_download_string = file_state_vec.download_string();
+                    message_vec.push(format!("{} <a href = \"/files/{}\">{}</a><br/>", source_ticket, file_state_vec_download_string, file_state_vec_download_string));
+                }
+
+                Response::builder()
+                    .status(StatusCode::OK)
+                    .body(message_vec.join("\n").into_bytes())
+            }
+        ).boxed()
+}
+
+fn get_files_endpoint<SystemType : System + Clone + Send + 'static>
+(cache : SysCache<SystemType>) -> warp::filters::BoxedFilter<(impl warp::Reply,)>
+{
+    warp::get()
         .and(warp::path!("files" / String))
         .map(move |hash_str : String|
             {
@@ -158,10 +185,14 @@ pub async fn serve
                     }
                 }
             }
-        );
+        )
+    .boxed()
+}
 
-    let history = elements.history.clone();
-    let rules_endpoint = warp::get()
+fn get_rules_endpoint<SystemType : System + Clone + Send + 'static>
+(history : History<SystemType>) -> warp::filters::BoxedFilter<(impl warp::Reply,)>
+{
+    warp::get()
         .and(warp::path!("rules" / String / String))
         .map(
             move |rule_hash_str : String, source_hash_str : String|
@@ -213,120 +244,151 @@ pub async fn serve
                 Response::builder()
                     .status(StatusCode::OK)
                     .body(format!("{}", target_tickets.download_string()).into_bytes())
-            });
+            })
+    .boxed()
+}
 
-    let home_endpoint =
+async fn process_upload(form: FormData) -> Result<impl Reply, Rejection>
+{
+    let mut parts = form.into_stream();
+
+    loop
     {
-        let history = elements.history.clone();
-        warp::get()
-            .and(warp::path!("history"))
-            .map(
-                move||
+        match parts.next().await
+        {
+            Some(part_result) =>
+            {
+                match part_result
                 {
-                    let message =
-                    match history.list()
+                    Ok(mut p) =>
                     {
-                        Ok(rule_hashes) =>
+                        println!("------Part Received-------");
+                        println!("name: {:?}", p.name());
+                        println!("filename: {:?}", p.filename());
+                        println!("content-type: {:?}", p.content_type());
+
+                        let target_filename =
+                        match p.filename()
                         {
-                            let mut message_vec = vec!["<html><head></head><body style = \"font-family: monospace;\">".to_string()];
-                            for hash in rule_hashes
+                            Some(name) =>
                             {
-                                message_vec.push(format!("<a href= \"rule_history/{}\">{}</a>", hash, hash));
+                                format!(".files/{}", name)
+                            },
+                            None =>
+                            {
+                                println!("Not a file actually");
+                                continue;
+                            },
+                        };
+
+                        println!("Making sure the .files directory is there...");
+                        match fs::create_dir(".files")
+                        {
+                            Ok(_) => {},
+                            Err(err) =>
+                            {
+                                eprintln!("create directory error: {}", err);
+                                warp::reject::reject();
                             }
-                            message_vec.push("</body></html>".to_string());
-                            message_vec.join("\n")
-                        },
-                        Err(error) =>
-                        {
-                            format!("{}", error)
                         }
-                    };
 
-                    Response::builder()
-                        .status(StatusCode::OK)
-                        .body(message)
-                }
-            )
-    };
+                        println!("proceeding with target_filename = {}", target_filename);
 
-    let rule_history_endpoint =
-    {
-        let history = elements.history.clone();
-        warp::get()
-            .and(warp::path!("rule_history" / String))
-            .map(
-                move|rule_hash_str : String|
-                {
-                    let rule_ticket =
-                    match Ticket::from_human_readable(&rule_hash_str)
-                    {
-                        Ok(ticket) => ticket,
-                        Err(error) =>
+                        match p.data().await
                         {
-                            return Response::builder()
-                                .status(StatusCode::NOT_FOUND)
-                                .body(format!("Error: {}", error).into_bytes())
-                        }
-                    };
+                            Some(p_result) =>
+                            {
+                                match p_result
+                                {
+                                    Ok(data_buf) =>
+                                    {
+                                        println!("{:?}", data_buf.remaining());
 
-                    let rule_history =
-                    match history.read_rule_history(&rule_ticket)
+                                        match fs::File::create(to_path_buf(&target_filename))
+                                        {
+                                            Ok(mut file) =>
+                                            {
+                                                println!("AH HA we're here, let's write the file!");
+                                                println!("{:?}", file);
+                                                let _ = std::io::copy(&mut data_buf.reader(), &mut file);
+                                            },
+                                            Err(err) =>
+                                            {
+                                                eprintln!("create file error: {}", err);
+                                                warp::reject::reject();
+                                            }
+                                        }
+                                    },
+
+                                    Err(err) =>
+                                    {
+                                        println!("error getting databuff: {:?}", err);
+                                    },
+                                }
+                            },
+
+                            None =>
+                            {
+                                println!("nodata");
+                            }
+                        };
+                    },
+
+                    Err(err) =>
                     {
-                        Ok(rule_history) => rule_history,
-                        Err(error) => return
-                            Response::builder()
-                                .status(StatusCode::NOT_FOUND)
-                                .body(format!("Error: {}", error).into_bytes()),
-                    };
-
-                    let mut message_vec = vec!["<html><head></head><body style = \"font-family: monospace;\">".to_string()];
-                    let source_to_targets = &rule_history.get_source_to_targets();
-
-                    for (source_ticket, file_state_vec) in source_to_targets.iter()
-                    {
-                        let file_state_vec_download_string = file_state_vec.download_string();
-                        message_vec.push(format!("{} <a href = \"/files/{}\">{}</a><br/>", source_ticket, file_state_vec_download_string, file_state_vec_download_string));
+                        println!("{:?}", err);
                     }
-
-                    Response::builder()
-                        .status(StatusCode::OK)
-                        .body(message_vec.join("\n").into_bytes())
                 }
-            )
-    };
+            },
 
-    let upload_file =
+            None => break,
+        }
+    }
+
+    Ok("success\n")
+}
+
+#[tokio::main]
+pub async fn serve
+<
+    SystemType : System + Clone + Send + 'static,
+>
+(
+    mut system : SystemType,
+    directory_path : &str,
+    address : Ipv4Addr,
+    port : u16
+) -> Result<(), ServerError>
+{
+    let elements =
+    match directory::init(&mut system, directory_path)
     {
-        let history = elements.history.clone();
-        warp::post()
-            .and(warp::multipart::form())
-            .map(
-                move|form: FormData|
-                {
-                    println!("upload received!");
-                    let mut message_vec = vec!["".to_string()];
-                    block_on(process_upload(cache, form));
-
-                    Response::builder()
-                        .status(StatusCode::OK)
-                        .body(message_vec.join("\n").into_bytes())
-                }
-            )
+        Ok(elements) => elements,
+        Err(error) => panic!("Failed to init directory error: {}", error)
     };
+
+    let history_endpoint = get_history_endpoint(elements.history.clone());
+    let rule_history_endpoint = get_rule_history_endpoint(elements.history.clone());
+    let files_endpoint = get_files_endpoint(elements.cache);
+    let rules_endpoint = get_rules_endpoint(elements.history);
+
+    let upload_route = warp::path("upload")
+        .and(warp::post())
+        .and(warp::multipart::form().max_length(5_000_000))
+        .and_then(process_upload);
 
     let socket_address = SocketAddr::new(IpAddr::V4(address), port);
     println!("Serving on {}", socket_address);
 
-    warp::serve(home_endpoint
-            .or(files_endpoint)
-            .or(rules_endpoint)
-            .or(rule_history_endpoint)
-            .or(upload_file)
-        )
-        .run(socket_address)
-        .await;
+    warp::serve(
+        upload_route
+        .or(history_endpoint)
+        .or(rule_history_endpoint)
+        .or(files_endpoint)
+        .or(rules_endpoint))
+    .run(socket_address).await;
 
-    Err(ServerError::Weird) 
+    Err(ServerError::Weird)
 }
 
 #[cfg(test)]
